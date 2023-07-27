62d34432e8e67bdd1da655bf4f4465e524bfe449
Nick <delta2888@gmail.com>
2023-07-27 16:28:28 +1000

    refactor: remove example function in db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,15 +2,3 @@ import { PrismaClient } from '@prisma/client';
 import 'server-only';
 
 export const prisma = new PrismaClient();
-
-export default async function prismaExample() {
-  const newUser = await prisma.user.create({
-    data: {
-      userId: '123',
-    },
-  });
-
-  const users = await prisma.user.findMany();
-
-  return { newUser, users };
-}
