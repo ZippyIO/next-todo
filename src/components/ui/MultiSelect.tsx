48e8775a2e805eab0cf2a59747cca942260be554
Nick <delta2888@gmail.com>
2023-08-01 10:50:51 +1000

    feat: add MultiSelect component - CreatableSelect
    - uses CreatableSelect from react-select with multi select option to allow multiple selected options & creatable options

---
+++ src/components/ui/MultiSelect.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import clsx from 'clsx';
+import CreatableSelect from 'react-select/creatable';
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface Props {
+  defaultValue?: Option | null;
+  options: Option[];
+  onValueChange: (value: string[]) => void;
+}
+
+const MultiSelect = ({ defaultValue, onValueChange, options }: Props) => {
+  const onChange = (option: readonly Option[]) => {
+    const values = option.map(({ value }) => value);
+    console.log(values);
+    onValueChange(values);
+  };
+  return (
+    <CreatableSelect
+      defaultValue={defaultValue}
+      onChange={onChange}
+      options={options}
+      isMulti
+      closeMenuOnSelect={false}
+      hideSelectedOptions={false}
+      unstyled
+      classNames={{
+        control: () => 'bg-zinc-900 p-2 rounded-md w-[250px] border-none',
+        valueContainer: () => 'gap-2',
+        multiValue: () => 'bg-blue-500 p-1 rounded-md text-xs',
+        multiValueLabel: () => 'text-white',
+        menu: () => 'bg-zinc-900 rounded-md overflow-hidden',
+        option: ({ isFocused, isSelected }) =>
+          clsx(isSelected && '!bg-blue-500', isFocused && '!bg-zinc-800', 'bg-zinc-900 p-2'),
+      }}
+    />
+  );
+};
+
+export default MultiSelect;
