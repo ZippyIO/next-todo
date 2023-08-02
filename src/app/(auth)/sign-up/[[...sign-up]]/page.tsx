c03b08698ad4f4e5f266b3aecd301bb73b3f610d
Nick <delta2888@gmail.com>
2023-08-02 12:39:53 +1000

    feat: add styling to sign-in & sign-up components

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,21 @@ import { SignUp } from '@clerk/nextjs';
 const Page = () => {
   return (
     <main className="mt-16 flex w-full justify-center">
-      <SignUp />
+      <SignUp
+        appearance={{
+          elements: {
+            card: 'bg-black',
+            headerTitle: 'text-zinc-100',
+            headerSubtitle: 'text-zinc-400',
+            socialButtonsBlockButton__discord:
+              'bg-zinc-900 hover:opacity-90 hover:bg-zinc-900 active:opacity-75 active:bg-zinc-900',
+            socialButtonsBlockButtonText__discord: 'text-indigo-400 font-medium',
+            socialButtonsBlockButtonArrow__discord: 'text-white',
+            footerActionText: 'text-zinc-400',
+            footerActionLink: 'text-blue-600 hover:text-blue-700',
+          },
+        }}
+      />
     </main>
   );
 };
