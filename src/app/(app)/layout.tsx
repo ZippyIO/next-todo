88c1aebf204bfb7e999bc27bc338a15ed3add3a2
Nick <delta2888@gmail.com>
2023-08-02 13:30:01 +1000

    feat: add shadcn/ui Toast, Toaster & use-toast

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,12 @@
 import Sidebar from '~/components/ui/Sidebar';
+import { Toaster } from '~/components/ui/Toaster';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen w-full bg-[#0a0a0b] bg-[url('/grid.svg')]">
       <Sidebar />
       {children}
+      <Toaster />
     </div>
   );
 };
